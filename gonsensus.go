1cf7005257f2e3b670982a36e6cfe4f005c02198
Ville Vesilehto <ville@vesilehto.fi>
2024-10-27 08:48:33 +0200

    newline

--- gonsensus.go
+++ gonsensus.go
@@ -32,8 +32,10 @@ const (
 
 // S3Client defines the interface for S3 operations.
 type S3Client interface {
-	PutObject(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error)
-	GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
+	PutObject(ctx context.Context, params *s3.PutObjectInput,
+		optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error)
+	GetObject(ctx context.Context, params *s3.GetObjectInput,
+		optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
 	DeleteObject(ctx context.Context, params *s3.DeleteObjectInput,
 		optFns ...func(*s3.Options)) (*s3.DeleteObjectOutput, error)
 }
