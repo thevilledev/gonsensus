3707910b9cd78922bedb905302644c03081730c0
Ville Vesilehto <ville@vesilehto.fi>
2024-10-29 20:59:13 +0200

    feat: add bash

---
+++ hack/scripts/panic.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo "🚨 CONSENSUS EMERGENCY DETECTED 🚨"
+sleep 1
+
+echo "
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡖⠁⠀⠀⠀⠀⠀⠀⠈⢲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⢀⣀⣤⣤⣤⣤⣀⡀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣔⢿⡿⠟⠛⠛⠻⢿⡿⣢⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣷⣤⣀⡀⢀⣀⣤⣾⣿⣿⣿⣷⣶⣤⡀⠀⠀⠀⠀
+⠀⠀⢠⣾⣿⡿⠿⠿⠿⣿⣿⣿⣿⡿⠏⠻⢿⣿⣿⣿⣿⠿⠿⠿⢿⣿⣷⡀⠀⠀
+⠀⢠⡿⠋⠁⠀⠀⢸⣿⡇⠉⠻⣿⠇⠀⠀⠸⣿⡿⠋⢰⣿⡇⠀⠀⠈⠙⢿⡄⠀
+⠀⡿⠁⠀⠀⠀⠀⠘⣿⣷⡀⠀⠰⣿⣶⣶⣿⡎⠀⢀⣾⣿⠇⠀⠀⠀⠀⠈⢿⠀
+⠀⡇⠀⠀⠀⠀⠀⠀⠹⣿⣷⣄⠀⣿⣿⣿⣿⠀⣠⣾⣿⠏⠀⠀⠀⠀⠀⠀⢸⠀
+⠀⠁⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⢇⣿⣿⣿⣿⡸⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠈⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠐⢤⣀⣀⢀⣀⣠⣴⣿⣿⠿⠋⠙⠿⣿⣿⣦⣄⣀⠀⠀⣀⡠⠂⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠉⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠋⠁⠀⠀⠀⠀⠀
+"
+sleep 1
+
+echo "🔍 Detecting leadership status..."
+sleep 1
+
+# Random leadership status
+if [ $((RANDOM % 2)) -eq 0 ]; then
+    echo "😱 SPLIT BRAIN DETECTED! PANIC MODE ACTIVATED!"
+    sleep 1
+    echo "👻 Ghost nodes detected in the cluster!"
+    sleep 1
+    echo "🧟 Zombie processes rising from the dead..."
+else
+    echo "🎉 You are the chosen leader!"
+    sleep 1
+    echo "👑 All hail the consensus king/queen!"
+    sleep 1
+    echo "🎭 Other nodes are your loyal subjects"
+fi
+
+sleep 1
+echo "
+🎲 Random consensus facts:
+- 9 out of 10 nodes prefer distributed systems over monoliths
+- A quorum of gophers is called a 'consensus'
+- S3 stands for Super Secure Storage (trust me, bro)
+"
+
+# Random failure message
+FAILURES=(
+    "Error 418: I'm a teapot, can't achieve consensus while brewing"
+    "Error 42: The answer to distributed consensus is unknown"
+    "Error 404: Consensus not found, have you tried turning it off and on again?"
+    "Error 500: Internal server error, the nodes are having an existential crisis"
+)
+RANDOM_FAILURE=${FAILURES[$RANDOM % ${#FAILURES[@]}]}
+
+sleep 1
+echo "⚠️ $RANDOM_FAILURE"
+sleep 1
+echo "🏃 Running away from distributed systems..."
+
+exit $((RANDOM % 2))  # Random success/failure
